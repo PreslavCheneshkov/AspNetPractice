23cedf531b3aab0e01345e7a2ab62dbb0f2fbc98
PreslavCheneshkov <101942358+PreslavCheneshkov@users.noreply.github.com>
2022-09-26 20:26:57 +0300

    innitial

---
+++ CarOffers/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarOffers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
