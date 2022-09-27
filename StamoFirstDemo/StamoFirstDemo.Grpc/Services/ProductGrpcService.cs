e53f648e2931c517829c7d584a1468aa49dbc72b
PreslavCheneshkov <101942358+PreslavCheneshkov@users.noreply.github.com>
2022-09-27 21:34:22 +0300

    added grpc

---
+++ StamoFirstDemo/StamoFirstDemo.Grpc/Services/ProductGrpcService.cs
@@ -0,0 +1,30 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using StamoFirstDemo.Core.Contracts;
+
+namespace StamoFirstDemo.Grpc.Services
+{
+    public class ProductGrpcService : Product.ProductBase
+    {
+        private readonly IProductService productService;
+        public ProductGrpcService(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        public override async Task<ProductList> GetAll(Empty request, ServerCallContext context)
+        {
+            ProductList result = new ProductList();
+            var products = await productService.GetAll();
+
+            result.Items.AddRange(products.Select(p => new ProductItem()
+            {
+                Name = p.Name,
+                Id = p.Id.ToString(),
+                Price = (double)p.Price,
+                Quantity = p.Quantity
+            }));
+            return result;
+        }
+    }
+}
